de8145ee8b73852e6038c8c7d51db7a7c1a0414d
SebaaK <sebaa.kot@gmail.com>
2022-06-01 22:34:00 +0200

    first

---
+++ controllers/photos.controller.js
@@ -0,0 +1,62 @@
+const Photo = require('../models/photo.model');
+
+/****** SUBMIT PHOTO ********/
+
+exports.add = async (req, res) => {
+
+  try {
+    const { title, author, email } = req.fields;
+    const file = req.files.file;
+
+    if(title && author && email && file) { // if fields are not empty...
+      if(title.length > 25)
+        throw new Error("Title have to much chars. Max chars is 25");
+      if(author.length > 50)
+        throw new Error("Author have to much chars. Max chars is 50");
+
+      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+      if(fileName.split('.').slice(-1)[0] !== 'jpg')
+        throw new Error("Wrong file!");
+      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+      await newPhoto.save(); // ...save new photo in DB
+      res.json(newPhoto);
+
+    } else {
+      throw new Error('Wrong input!');
+    }
+
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+};
+
+/****** LOAD ALL PHOTOS ********/
+
+exports.loadAll = async (req, res) => {
+
+  try {
+    res.json(await Photo.find());
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+};
+
+/****** VOTE FOR PHOTO ********/
+
+exports.vote = async (req, res) => {
+
+  try {
+    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    else {
+      photoToUpdate.votes++;
+      photoToUpdate.save();
+      res.send({ message: 'OK' });
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+};
